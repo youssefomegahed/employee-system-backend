c7c55fb5f780002e8cb63d89e9b9dc2d17dd6e8b
Youssef Megahed <youssefosama@aucegypt.edu>
2023-07-30 23:31:50 +0300

    setup remote mongodb + /verifyHRUser api

--- index.js
+++ index.js
@@ -1,13 +1,53 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 
 const app = express();
 const port = 3000;
 
 app.use(cors());
-
 app.use(express.json());
 
+const Employee = require("./model/employee");
+
+mongoose.connect(
+  "mongodb+srv://youssefm:6ENb1zVVmhJeyATf@employee-system.ocuwita.mongodb.net/?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: "employee-system",
+  },
+
+  (err) => {
+    if (err) {
+      console.error("Error connecting to employee-system database:", err);
+    } else {
+      console.log("Connected to employee-system database");
+    }
+  }
+);
+
+/* APIs */
+app.post("/verifyHRUser", async (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  console.log(email, password);
+
+  try {
+    const user = await Employee.findOne({ email }).exec();
+
+    if (!user || user.group !== "HR" || user.password !== password) {
+      return res.json(false);
+    }
+
+    return res.json(user);
+  } catch (err) {
+    console.error("Error verifying HR user:", err);
+    return res.status(500).json({ error: "Internal server error" });
+  }
+});
+
 app.listen(port, () => {
-  console.log(`listening at port ${port}`);
+  console.log(`Listening at port ${port}`);
 });
---
+++ model/employee.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const { Schema, model } = mongoose;
+
+const employeeSchema = new Schema({
+  name: String,
+  email: String,
+  password: String,
+  group: String,
+});
+
+const Employee = model("Employee", employeeSchema);
+
+module.exports = Employee;
